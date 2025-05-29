f5e26a122c2b30c202e2edaae8d8c33ea0eed5ad
Youva LOUNAS <youva.lounas.97@gmail.com>
2025-05-29 21:20:11 +0200

    update ex3 and ex4

--- exercices/ex3/four-leds-flash.ino
+++ exercices/ex3/four-leds-flash.ino
@@ -5,23 +5,27 @@
 
 void setup()
 {
-    pinMode(PIN_13, OUTPUT);
-  	pinMode(PIN_12, OUTPUT);
-  	pinMode(PIN_11, OUTPUT);
-  	pinMode(PIN_10, OUTPUT);
-    Serial.begin(9600);
+	pinMode(PIN_13, OUTPUT);
+	pinMode(PIN_12, OUTPUT);
+	pinMode(PIN_11, OUTPUT);
+	pinMode(PIN_10, OUTPUT);
+    
+	Serial.begin(9600);
 }
 
 void loop()
 {
-    digitalWrite(PIN_13, HIGH);
-  	digitalWrite(PIN_12, HIGH);
-  	digitalWrite(PIN_11, HIGH);
-  	digitalWrite(PIN_10, HIGH);
-    delay(500); 
+	digitalWrite(PIN_13, HIGH);
+	digitalWrite(PIN_12, HIGH);
+	digitalWrite(PIN_11, HIGH);
+	digitalWrite(PIN_10, HIGH);
+    
+	delay(500); 
+	
 	digitalWrite(PIN_13, LOW);
-  	digitalWrite(PIN_12, LOW);
-  	digitalWrite(PIN_11, LOW);
-  	digitalWrite(PIN_10, LOW);
-  	delay(500);
+	digitalWrite(PIN_12, LOW);
+	digitalWrite(PIN_11, LOW);
+	digitalWrite(PIN_10, LOW);
+  	
+	delay(500);
 }
--- exercices/ex4/four-leds-chaser.ino
+++ exercices/ex4/four-leds-chaser.ino
@@ -6,26 +6,34 @@
 void setup()
 {
     pinMode(PIN_13, OUTPUT);
-  	pinMode(PIN_12, OUTPUT);
-  	pinMode(PIN_11, OUTPUT);
-  	pinMode(PIN_10, OUTPUT);
+    pinMode(PIN_12, OUTPUT);
+    pinMode(PIN_11, OUTPUT);
+    pinMode(PIN_10, OUTPUT);
+    
     Serial.begin(9600);
 }
 
 void loop()
 {
     digitalWrite(PIN_13, HIGH);
-  	digitalWrite(PIN_12, LOW);
-  	digitalWrite(PIN_11, LOW);
-  	digitalWrite(PIN_10, LOW);
+    digitalWrite(PIN_12, LOW);
+    digitalWrite(PIN_11, LOW);
+    digitalWrite(PIN_10, LOW);
+    
     delay(100); 
-	digitalWrite(PIN_13, LOW);
+    
+    digitalWrite(PIN_13, LOW);
     digitalWrite(PIN_12, HIGH);
-  	delay(100);
-  	digitalWrite(PIN_12, LOW);
+    
+    delay(100);
+    
+    digitalWrite(PIN_12, LOW);
     digitalWrite(PIN_11, HIGH);
-  	delay(100);
-  	digitalWrite(PIN_11, LOW);
+    
+    delay(100);
+    
+    digitalWrite(PIN_11, LOW);
     digitalWrite(PIN_10, HIGH);
-  	delay(100);
+    
+    delay(100);
 }
